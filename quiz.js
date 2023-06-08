b8532dc3f756132733ac90ef7068487aa9fc3626
FGoldeneye <135179187+FGoldeneye@users.noreply.github.com>
2023-06-08 15:21:19 +0200

    alles geupdate

--- quiz.js
+++ quiz.js
@@ -1,12 +1,12 @@
 //dit is de objectvorm voor het opslaan van de vragen
- const vragen = [
+const vragen = [
     {
         vraag: "Welke kleur is insectenbloed?",
         optieA: "lichtgeel",
         optieB: "rood",
         optieC: "lichtblauw",
         optieD: "donker wit",
-        goedantwoord: "optieA"
+        goedAntwoord: "optieA"
     },
 
     {
@@ -15,7 +15,7 @@
         optieB: "geen witte bloedcellen",
         optieC: "geen rode bloedcellen",
         optieD: "geen hemoglobine in bloed",
-        goedantwoord: "optieD"
+        goedAntwoord: "optieD"
     },
 
     {
@@ -24,7 +24,7 @@
         optieB: "600m",
         optieC: "40m",
         optieD: "500m",
-        goedantwoord: "optieB"
+        goedAntwoord: "optieB"
     },
 
     {
@@ -33,7 +33,7 @@
         optieB: "voor decoratie",
         optieC: "om prooi te lokken",
         optieD: "hij kan niet goed zien",
-        goedantwoord: "optieC"
+        goedAntwoord: "optieC"
     },
 
     {
@@ -42,7 +42,7 @@
         optieB: "knobbelkiezen",
         optieC: "ze hebben geen tanden",
         optieD: "hoektanden",
-        goedantwoord: "optieD"
+        goedAntwoord: "optieD"
     },
 
     {
@@ -51,7 +51,7 @@
         optieB: "korter verteringsstelsel",
         optieC: "toevallig",
         optieD: "een koe eet meer",
-        goedantwoord: "optieB"
+        goedAntwoord: "optieB"
     },
 
     {
@@ -60,7 +60,7 @@
         optieB: "diereneter",
         optieC: "alleseter",
         optieD: "graseter",
-        goedantwoord: "optieA"
+        goedAntwoord: "optieA"
     },
 
     {
@@ -69,7 +69,7 @@
         optieB: "mimicry",
         optieC: "dieren aanvallen",
         optieD: "ondergronds blijven",
-        goedantwoord: "optieB"
+        goedAntwoord: "optieB"
     },
 
     {
@@ -78,7 +78,7 @@
         optieB: "in zijn stinger",
         optieC: "door zijn hele lichaan",
         optieD: "in zijn poten",
-        goedantwoord: "optieC"
+        goedAntwoord: "optieC"
     },
 
     {
@@ -87,7 +87,7 @@
         optieB: "in de middag",
         optieC: "in de avond",
         optieD: "in de nacht",
-        goedantwoord: "optieB"
+        goedAntwoord: "optieB"
     },
 
     {
@@ -96,111 +96,108 @@
         optieB: "in de middag",
         optieC: "in de avond",
         optieD: "in de nacht",
-        goedantwoord: "optieD"
+        goedAntwoord: "optieD"
     }
 ];
 
 
-let geschuddevragen = [] //lege array om geschudde geselecteerde vragen uit alle beschikbare vragen te houden
+let geschuddeVragen = [] //lege array om geschudde geselecteerde vragen uit alle beschikbare vragen te houden
 
-function behandelvragen() { 
-    //functie om te shufflen en 10 vragen naar geschuddevragen array te pushen
+function behandelVragen() {
+    //functie om te shufflen en 10 vragen naar geschuddeVragen array te pushen
     //app zou 10 vragen per sessie behandelen
-    while (geschuddevragen.length <= 9) {
-        const random = vragen[Math.floor(Math.random() * vragen.length)]
-        if (!geschuddevragen.includes(random)) {
-            geschuddevragen.push(random)
+    for (let i = 0; i < 10; i++) {
+        const random = vragen[Math.floor(Math.random() * vragen.length)];
+        if (!geschuddeVragen.includes(random)) {
+            geschuddeVragen.push(random);
         }
     }
 }
 
+function timeoutInstellen(callback, delay) {
+    setTimeout(callback, delay);
+}  
+
 
 let vraagNummer = 1 
 let spelerScoor = 0 
-let foutPogingen = 0
-let indexNumber = 0 
+let fouten = 0
+let indexNummer = 0 
 
 // Functie om de volgende vraag in de array te tonen
-function Volgendevraag(index) {
-    behandelvragen()
-    const huidigvraag = geschuddevragen[index]
+function VolgendeVraag(index) {
+    behandelVragen()
+    const huidigVraag = geschuddeVragen[index]
     document.getElementById("vraag-nummer").innerHTML = vraagNummer
     document.getElementById("speler-scoor").innerHTML = spelerScoor
-    document.getElementById("vraag-weergeven").innerHTML = huidigvraag.vraag;
-    document.getElementById("optie-een-label").innerHTML = huidigvraag.optieA;
-    document.getElementById("optie-twee-label").innerHTML = huidigvraag.optieB;
-    document.getElementById("optie-drie-label").innerHTML = huidigvraag.optieC;
-    document.getElementById("optie-vier-label").innerHTML = huidigvraag.optieD;
+    document.getElementById("vraag-weergeven").innerHTML = huidigVraag.vraag;
+    document.getElementById("optie-een-label").innerHTML = huidigVraag.optieA;
+    document.getElementById("optie-twee-label").innerHTML = huidigVraag.optieB;
+    document.getElementById("optie-drie-label").innerHTML = huidigVraag.optieC;
+    document.getElementById("optie-vier-label").innerHTML = huidigVraag.optieD;
 
 }
 
 
 function controleerVoorAntwoord() {
-    const huidigvraag = geschuddevragen[indexNumber] //pakt huidige vraag
-    const huidigvraagAntwoord = huidigvraag.goedantwoord //pakt huidige antwoord
-    const opties = document.getElementsByName("optie"); //pakt alle opties
-    let goedantwoord = null
+    const huidigVraag = geschuddeVragen[indexNummer];
+    const huidigVraagAntwoord = huidigVraag.goedAntwoord;
+    const opties = document.getElementsByName("optie");
+    let goedAntwoord = null;
 
     opties.forEach((optie) => {
-        if (optie.value === huidigvraagAntwoord) {
-            goedantwoord = optie.labels[0].id
+        if (optie.value === huidigVraagAntwoord) {
+            goedAntwoord = optie.labels[0].id;
         }
-    })
+    });
 
-    //controleren of een radio-ingang is gecontroleerd of een optie is gekozen
     if (opties[0].checked === false && opties[1].checked === false && opties[2].checked === false && opties[3].checked == false) {
-        document.getElementById('optie-modaal').style.display = "flex"
+        document.getElementById('optie-modaal').style.display = "flex";
     }
 
-    //controleren of keuze optie hetzelfde is als antwoord
     opties.forEach((optie) => {
-        if (optie.checked === true && optie.value === huidigvraagAntwoord) {
-            document.getElementById(goedantwoord).style.backgroundColor = "green"
-            spelerScoor++ //toevoegen aan de scoor van de speler
-            indexNumber++ //1 toevoegen aan index dus moet volgende vraag weergeven
-            //ingesteld om vraagnummer uit te stellen tot wanneer de volgende vraag wordt geladen
-            
-            timeoutinstellen(() => {
-                vraagNummer++
-            }, 1000)
+        if (optie.checked === true && optie.value === huidigVraagAntwoord) {
+            document.getElementById(goedAntwoord).style.backgroundColor = "green";
+            spelerScoor++;
+            indexNummer++;
+            timeoutInstellen(() => {
+                vraagNummer++;
+            }, 1000);
+        } else if (optie.checked && optie.value !== huidigVraagAntwoord) {
+            const foutLabelId = optie.labels[0].id;
+            document.getElementById(foutLabelId).style.backgroundColor = "red";
+            document.getElementById(goedAntwoord).style.backgroundColor = "green";
+            fouten++;
+            indexNummer++;
+            timeoutInstellen(() => {
+                vraagNummer++;
+            }, 1000);
         }
-
-        else if (optie.checked && optie.value !== huidigvraagAntwoord) {
-            const foutLabelId = optie.labels[0].id
-            document.getElementById(foutLabelId).style.backgroundColor = "red"
-            document.getElementById(goedantwoord).style.backgroundColor = "green"
-            foutPogingen++ //telt 1 op bij foute pogingen
-            indexNumber++
-            //ingesteld om vraagnummer uit te stellen tot wanneer de volgende vraag wordt geladen
-            
-            timeoutinstellen(() => {
-                vraagNummer++
-            }, 1000)
-        }
-    })
+    });
 }
+
 
 
 //opgeroepen wanneer de volgende knop wordt aangeroepen
 function behandelVolgendeVraag() {
     controleerVoorAntwoord() //controleer of de speler de juiste of verkeerde optie heeft gekozen
-    oncontroleerRadioKnoppen()
+    onControleerRadioKnoppen()
     //vertraagt ​​de weergave van de volgende vraag een seconde, alleen voor sommige effecten, zodat vragen niet op de speler afstormen
     
-    timeoutinstellen(() => {
-        if (indexNumber <= 9) {
+    timeoutInstellen(() => {
+        if (indexNummer <= 9) {
             //geeft volgende vraag weer zolang indexnummer niet groter is dan 9, onthoud dat indexnummer begint bij 0, dus index 9 is vraag 10
-            Volgendevraag(indexNumber)
+            VolgendeVraag(indexNummer)
         }
         else {
             behandelEindSpel()//beëindigt het spel als indexgetal groter is dan 9, wat betekent dat we al bij de 10e vraag zijn
         }
-        resetoptieAchtergrond()
+        resetOptieAchtergrond()
     }, 1000);
 }
 
 //zet opties background terug op null na weergave juiste/verkeerde kleuren
-function resetoptieAchtergrond() {
+function resetOptieAchtergrond() {
     const opties = document.getElementsByName("optie");
     opties.forEach((optie) => {
         document.getElementById(optie.labels[0].id).style.backgroundColor = ""
@@ -208,7 +205,7 @@ function resetoptieAchtergrond() {
 }
 
 // alle radio knoppen oncontroleren voor volgende vraag(kan ook worden gedaan met map of voorelk loop)
-function oncontroleerRadioKnoppen() {
+function onControleerRadioKnoppen() {
     const opties = document.getElementsByName("optie");
     for (let i = 0; i < opties.length; i++) {
         opties[i].checked = false;
@@ -233,30 +230,28 @@ function behandelEindSpel() {
         opmerking = "Goed gedaan!"
         opmerkingKleur = "green"
     }
-    const spelerCijfer = (spelerScoor / 10)
 
     //gegevens om weer te geven op het scorebord
     document.getElementById('opmerkingen').innerHTML = opmerking
     document.getElementById('opmerkingen').style.color = opmerkingKleur
-    document.getElementById('cijfer').innerHTML = spelerCijfer
-    document.getElementById('fout-antwoorden').innerHTML = foutPogingen
-    document.getElementById('goed-antwoorden').innerHTML = spelerScoor
+    document.getElementById('cijfer').innerHTML = spelerScoor
+    document.getElementById('fout-antwoorden').innerHTML = fouten
     document.getElementById('scoor-modaal').style.display = "flex"
 
 }
 
 //sluit scoor modaal, opnieuw spel laten beginnen en herschudt vragen
-function sluitscoorModaal() {
+function sluitScoorModaal() {
     vraagNummer = 1
     spelerScoor = 0
-    foutPogingen = 0
-    indexNumber = 0
-    geschuddevragen = []
-    Volgendevraag(indexNumber)
+    fouten = 0
+    indexNummer = 0
+    geschuddeVragen = []
+    VolgendeVraag(indexNummer)
     document.getElementById('scoor-modaal').style.display = "none"
 }
 
 //function to sluit warning modaal
-function sluitoptieModaal() {
+function sluitOptieModaal() {
     document.getElementById('optie-modaal').style.display = "none"
 }
